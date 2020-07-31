06ea230115ed8fd79c2e9ba390acbb544ff460f8
gabi <gabi@blend.com>
2020-07-31 10:01:23 -0700

    div for modal

--- src/react/org-chart.js
+++ src/react/org-chart.js
@@ -1,13 +1,36 @@
 const { createElement, PureComponent } = require('react')
 const { init } = require('../chart')
+const { divide } = require('lodash')
 
 class OrgChart extends PureComponent {
   render() {
+    console.log("in org-chart");
     const { id } = this.props
 
-    return createElement('div', {
-      id
-    })
+    const newDiv = createElement('div', {
+      id,
+    });
+
+    const myModal = createElement('div', {
+      id: 'myModal',
+      style: {
+        backgroundColor: '#fafafa',
+        width: 500,
+        height: 500,
+        position: 'absolute',
+        borderRadius: 5,
+        border: '1px solid #473c3c',
+      },
+    });
+    console.log("myModal: ", myModal);
+    const outerChart = createElement('div', {
+      id: "OuterChart",
+      height: 1000,
+      padding: 15,
+      className: 'App-contentWrapper',
+    },
+    [myModal, newDiv]);
+    return outerChart;
   }
 
   static defaultProps = {
